33578773cfabada5a87ee811f5343a19f467b9ca
carson-jenkins <cjenkin5@uccs.edu>
2023-09-19 14:57:12 -0600

    Edit views file

--- portfolio/portfolio_app/views.py
+++ portfolio/portfolio_app/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
 def index(request):
 
-# Render the HTML template index.html with the data in the context variable.
-    return HttpResponse('home page')
+# Render index.html
+
+    return render( request, 'portfolio_app/index.html')

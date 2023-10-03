40efa8b1aa4bcf695a8f4dfaa4bfa1a09011f63a
carson-jenkins <cjenkin5@uccs.edu>
2023-10-02 18:52:18 -0600

    First commit

--- portfolio/portfolio_app/admin.py
+++ portfolio/portfolio_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Student, Portfolio
 
-# Register your models here.
+# Define a custom admin class for Portfolio
+class PortfolioAdmin(admin.ModelAdmin):
+    list_display = ('title', 'contact_email', 'is_active')
+    search_fields = ('title', 'contact_email')
+
+# Register your models with the custom admin classes
+admin.site.register(Student)
+admin.site.register(Portfolio, PortfolioAdmin)

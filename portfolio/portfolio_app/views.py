0ae81b6bab32b8898d181fa5c171384da30fc23e
carson-jenkins <cjenkin5@uccs.edu>
2023-10-12 10:36:59 -0600

    Add html files

--- portfolio/portfolio_app/views.py
+++ portfolio/portfolio_app/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import *
+from django.views import generic
+# from .forms import ProjectForm, PortfolioForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -8,3 +12,8 @@ def index(request):
 # Render index.html
 
     return render( request, 'portfolio_app/index.html')
+
+class StudentListView(generic.ListView):
+    model = Student
+class StudentDetailView(generic.DetailView):
+    model = Student

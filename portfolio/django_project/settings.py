e7159cf3ffe09c6b1244fdb35bb7c7b70dfa95eb
carson-jenkins <cjenkin5@uccs.edu>
2023-10-19 23:31:47 -0600

    GE05

--- portfolio/django_project/settings.py
+++ portfolio/django_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-#km*-x5er_b5cqzc%ue_+6+xhic4+3k4x3*t&gs=ph!1ubjoz#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
